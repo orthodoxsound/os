49969a0ae01b4f5a61aa94837cc862584ad96fac
orthodoxsound <easternorthodox451@gmail.com>
2025-05-01 06:04:16 +0300

    discord.js

--- discord.js
+++ discord.js
@@ -1,8 +1,15 @@
 fetch('https://discord-backend-npqh.onrender.com/server-info')
   .then(res => res.json())
   .then(data => {
+    const memberList = data.members.join(', ');
+    const channelList = data.channels.join(', ');
+
     document.getElementById('discord-info').innerText =
-      `Server: ${data.name}\nMembers: ${data.memberCount}`;
+      `Discord Server: ${data.name}\n` +
+      `Online Members: ${data.memberCount}\n` +
+      `Invite: ${data.invite}\n\n` +
+      `Channels: ${channelList}\n\n` +
+      `Online Now: ${memberList}`;
   })
   .catch(err => {
     document.getElementById('discord-info').innerText = 'Failed to load server info';
